6ed784726a448104fc08f989294fbd4c4ed84c15
Vikrant-98 <vikrantchitte5398@gmail.com>
2021-01-25 15:20:03 +0530

    Delete Node

--- Ashu/Program.cs
+++ Ashu/Program.cs
@@ -19,7 +19,45 @@ namespace Ashu
 
         }
 
-        void InsertAtLast(int val) 
+        private void Insert() 
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1 Insert At Last :");
+            Console.WriteLine("2 Insert at First :");
+            Console.WriteLine("3 Insert at Position :");
+            Console.Write("Enter your Choice :");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---------------------------------------------");
+            switch (choice)
+            {
+                case 1:
+                    {
+                        InsertAtLast(input("Enter your Value:"));
+                        break;
+                    }
+                case 2:
+                    {
+                        InsertAtFirst(input("Enter your Value:"));
+                        break;
+                    }
+                case 3:
+                    {
+                        InsertAtPerticularPlace(
+                                input("Enter your Position where you want to insert:"),
+                                input("Enter your Value:"
+                                )
+                            );
+                        break;
+                    }
+                default: 
+                    {
+                        Console.WriteLine("Enter Valid Input");
+                        break;
+                    }
+            }
+        }
+
+        private void InsertAtLast(int val) 
         {
             Node node = new Node(val);
             Node node2 = null;
@@ -37,31 +75,10 @@ namespace Ashu
                 }
                 node2.next = node;   
             }
-            NodesCount();
+            DisplayNodesCount();
         }
 
-        void Display() 
-        {
-            Node node1 = Head;
-            NodeCount = 0;
-            if (node1 != null)
-            {
-                while (node1.next != null)
-                {
-                    Console.Write(" --> [ " + node1.data + " ] [Next] ");
-                    node1 = node1.next;
-                }
-                Console.Write(" -->  [ " + node1.data + " ] [Null] ");
-                NodesCount();
-            }
-            else 
-            {
-                Console.WriteLine("Ooops Node is Empty");
-                NodesCount();
-            }
-        }
-
-        void InsertAtFirst(int val) 
+        private void InsertAtFirst(int val)
         {
             Node node = new Node(val);
             Node node2 = null;
@@ -75,27 +92,14 @@ namespace Ashu
                 node2.next = Head;
                 Head = node2;
             }
-            NodesCount();
+            DisplayNodesCount();
         }
 
-        void UpdateValue(int val,int change) 
+        private void InsertAtPerticularPlace(int Position, int val)
         {
-            Node node = Head;
-            if (node != null)
-            {
-                while (node.data != val && node.next != null)
-                {
-                    node = node.next;
-                }
-                node.data = change;
-            }
-        }
-
-        void InsertAtPerticularPlace(int Position,int val) 
-        {
-            NodesCount();
+            DisplayNodesCount();
             int temp = 1;
-            if (Position > 0 && Position < NodeCount) 
+            if (Position > 0 && Position < NodeCount)
             {
                 if (Position == 1)
                 {
@@ -117,10 +121,33 @@ namespace Ashu
                     node.next = node1;
                 }
             }
-            NodesCount();
+            DisplayNodesCount();
         }
 
-        void NodesCount() 
+        private void Display() 
+        {
+            Node node1 = Head;
+            NodeCount = 0;
+            if (node1 != null)
+            {
+                Console.WriteLine("--------------------------------------------------");
+                while (node1.next != null)
+                {
+                    Console.Write(" --> [ " + node1.data + " ] [Next] ");
+                    node1 = node1.next;
+                }
+                Console.Write(" -->  [ " + node1.data + " ] [Null]\n");
+                Console.WriteLine("--------------------------------------------------");
+                DisplayNodesCount();
+            }
+            else 
+            {
+                Console.WriteLine("Ooops Node is Empty");
+                DisplayNodesCount();
+            }
+        }
+
+        private void DisplayNodesCount()
         {
             Node node1 = Head;
             NodeCount = 0;
@@ -129,7 +156,64 @@ namespace Ashu
                 node1 = node1.next;
                 NodeCount++;
             }
+            NodeCount++;
             Console.WriteLine("Total Node is " + NodeCount);
+        }
+
+        private void UpdateValue(int val,int change) 
+        {
+            Node node = Head;
+            if (node != null)
+            {
+                while (node.data != val && node.next != null)
+                {
+                    node = node.next;
+                }
+                node.data = change;
+            }
+        }
+
+        private void DeleteNote() 
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1 Delete by value :");
+            Console.WriteLine("2 Insert at First :");
+            Console.Write("Enter your Choice :");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---------------------------------------------");
+            switch (choice)
+            {
+                case 1: 
+                    {
+                        DeleteNote(input("Enter your Value:"));
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter Valid Input");
+                        break;
+                    }
+            }
+        }
+
+        private void DeleteNote(int val)
+        {
+            Node node = Head;
+            if (node != null)
+            {
+                while (node.next.data != val && node.next != null)
+                {
+                    node = node.next;
+                }
+                node = node.next.next;
+            }
+        }
+
+        private int input(string text) 
+        {
+            Console.WriteLine(text);
+            int val = Convert.ToInt32(Console.ReadLine());
+            return val;
         }
 
         static void Main(string[] args)
@@ -137,56 +221,49 @@ namespace Ashu
             Program prg = new Program();
             while (1 != 0)
             {
-                Console.WriteLine("Enter your Choice :");
+                Console.WriteLine("---------------------------------------------");
                 Console.WriteLine("1 Insert :");
-                Console.WriteLine("2 Insert at First :");
-                Console.WriteLine("3 Insert at PerticularNode :");
-                Console.WriteLine("4 Update :");
-                Console.WriteLine("5 Display Node :");
-                Console.WriteLine("6 Display Node Count :");
+                Console.WriteLine("2 Update :");
+                Console.WriteLine("3 Delete :");
+                Console.WriteLine("4 Display Node :");
+                Console.WriteLine("5 Display Node Count :");
+                Console.Write("Enter your Choice : ");
                 int choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("---------------------------------------------");
                 switch (choice)
                 {
                     case 1: 
                         {
-                            Console.WriteLine("Enter your Value:");
-                            int val = Convert.ToInt32(Console.ReadLine());
-                            prg.InsertAtLast(val);
+                            prg.Insert();
                             break;
                         }
-                    case 2: 
+                    case 2:
                         {
-                            Console.WriteLine("Enter your Value:");
-                            int val = Convert.ToInt32(Console.ReadLine());
-                            prg.InsertAtFirst(val);
+                            
+                            prg.UpdateValue(
+                                prg.input("Enter your Value you want to change:"), 
+                                prg.input("Enter your Value you want to Insert:"
+                                ));
                             break;
                         }
-                    case 3:
+                    case 3: 
                         {
-                            Console.WriteLine("Enter your Value:");
-                            int val = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("Enter your Position where you want to insert:");
-                            int Position = Convert.ToInt32(Console.ReadLine());
-                            prg.InsertAtPerticularPlace(Position,val);
+                            prg.DeleteNote();
                             break;
                         }
                     case 4:
                         {
-                            Console.WriteLine("Enter your Value you want to change:");
-                            int val = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("Enter your Value you want to Insert:");
-                            int Change = Convert.ToInt32(Console.ReadLine());
-                            prg.UpdateValue(val, Change);
+                            prg.Display();
                             break;
                         }
                     case 5:
                         {
-                            prg.Display();
+                            prg.DisplayNodesCount();
                             break;
                         }
-                    case 6:
+                    default:
                         {
-                            prg.NodesCount();
+                            Console.WriteLine("Enter Valid Input");
                             break;
                         }
                 }
